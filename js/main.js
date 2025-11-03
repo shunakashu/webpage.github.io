0d4bd992e520a3ddba116d99d8a06eb5fe7d040c
shunakashu <shunakashu@gmail.com>
2025-11-03 17:15:36 +0900

    Add main.js

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+// debounce関数
+function debounce(func, wait) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            func.apply(context, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+
+//ハンバーガーメニューをクリックした際の処理
+$(function() {
+	$('#menubar_hdr').click(function() {
+		$(this).toggleClass('ham');
+
+			if($(this).hasClass('ham')) {
+				$('#menubar').addClass('d-b');
+			} else {
+				$('#menubar').removeClass('d-b');
+			}
+
+	});
+});
+
+
+// 同一ページへのリンクの場合に開閉メニューを閉じる処理
+$(function() {
+	$('#menubar a[href^="#"]').click(function() {
+		$('#menubar').removeClass('d-b');
+		$('#menubar_hdr').removeClass('ham');
+	});
+});
+
+
+// スムーススクロール（※バージョン2024-1）※通常タイプ
+$(function() {
+    // ページ上部へ戻るボタンのセレクター
+    var topButton = $('.pagetop');
+    // ページトップボタン表示用のクラス名
+    var scrollShow = 'pagetop-show';
+
+    // スムーススクロールを実行する関数
+    // targetにはスクロール先の要素のセレクターまたは'#'（ページトップ）を指定
+    function smoothScroll(target) {
+        // スクロール先の位置を計算（ページトップの場合は0、それ以外は要素の位置）
+        var scrollTo = target === '#' ? 0 : $(target).offset().top;
+        // アニメーションでスムーススクロールを実行
+        $('html, body').animate({scrollTop: scrollTo}, 500);
+    }
+
+    // ページ内リンクとページトップへ戻るボタンにクリックイベントを設定
+    $('a[href^="#"], .pagetop').click(function(e) {
+        e.preventDefault(); // デフォルトのアンカー動作をキャンセル
+        var id = $(this).attr('href') || '#'; // クリックされた要素のhref属性を取得、なければ'#'
+        smoothScroll(id); // スムーススクロールを実行
+    });
+
+    // スクロールに応じてページトップボタンの表示/非表示を切り替え
+    $(topButton).hide(); // 初期状態ではボタンを隠す
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= 300) { // スクロール位置が300pxを超えたら
+            $(topButton).fadeIn().addClass(scrollShow); // ボタンを表示
+        } else {
+            $(topButton).fadeOut().removeClass(scrollShow); // それ以外では非表示
+        }
+    });
+
+    // ページロード時にURLのハッシュが存在する場合の処理
+    if(window.location.hash) {
+        // ページの最上部に即時スクロールする
+        $('html, body').scrollTop(0);
+        // 少し遅延させてからスムーススクロールを実行
+        setTimeout(function() {
+            smoothScroll(window.location.hash);
+        }, 10);
+    }
+});
+
+
+// 汎用開閉処理
+$(function() {
+	$('.openclose').next().hide();
+	$('.openclose').click(function() {
+		$(this).next().slideToggle();
+		$('.openclose').not(this).next().slideUp();
+	});
+});
+
+
+// トップページのロゴのスクロール
+$(function() {
+    // 初期位置を中央に設定
+    var windowHeight = $(window).height();
+    var logoHeight = $("#logo").height();
+    var initialTop = (windowHeight - logoHeight) / 2;
+    $("#logo").css('top', initialTop + 'px');
+
+    $(window).scroll(function() {
+        // スクロール量を取得
+        var scrollTop = $(window).scrollTop();
+
+        // #logoのスクロール量の調整。ここの4を変更するとロゴに移動速度も変わります。
+        $("#logo").css('top', (initialTop - scrollTop / 4) + 'px');
+    });
+});
